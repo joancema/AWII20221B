cbd4d4422754805825d19a25907afb4b0d35cf76
John Cevallos <joancemac@gmail.com>
2022-05-10 14:44:47 -0500

    comentarios

--- 03mongodb/ejemplo.js
+++ 03mongodb/ejemplo.js
@@ -1,19 +1,20 @@
 const mongoose =  require("mongoose");
-const conexion=  "mongodb+srv://joancema:jacm1310@cluster0.x7met.mongodb.net/usuarios?retryWrites=true&w=majority";
+const conexion=  "mongodb+srv://joancema:jacm1310@cluster0.x7met.mongodb.net/aaaprueba?retryWrites=true&w=majority";
 
 (async ()=>{
-
+    //conectamos a la bd
     const conectado =  await mongoose.connect(conexion);
-    const Grupo = mongoose.model("Grupo", {nombre:String});
-    const grupo1 =  new Grupo({nombre:"Administradores"});
-    const Permiso = mongoose.model("Permiso", {nombre:String});
-    const permiso1 = new Permiso({nombre:"Guardar"});
-    const permiso2 = new Permiso({nombre:"Eliminar"});
-
+    //Definir los models
+    const Grupo = mongoose.model("Grupo", {
+        nombre:String,
+        // usuarios:[{ type: mongoose.Schema.Types.ObjectId, ref:"Usuario" }]
+    });
+    const Permiso = mongoose.model("Permiso", {nombre:String});    
     const Usuario =  mongoose.model("Usuario", 
     {
          nombre: String,
           idgrupo: { type: mongoose.Schema.Types.ObjectId, ref:"Grupo" },
+        //   permisos:[{type: mongoose.Schema.Types.ObjectId, ref:"Permiso" }],
           permisos: [
               {
                   permiso:{type: mongoose.Schema.Types.ObjectId, ref:"Permiso" },
@@ -23,18 +24,32 @@ const conexion=  "mongodb+srv://joancema:jacm1310@cluster0.x7met.mongodb.net/usu
             ],
           
     });
+    //Instancias de los modelos
+    const grupo1 =  new Grupo({nombre:"Administradores"});
+    const permiso1 = new Permiso({nombre:"Guardar"});
+    const permiso2 = new Permiso({nombre:"Eliminar"});
+    //Almacenar grupo y permisos
     const grupoAlmacenado =  await grupo1.save();
-    //todo guardarPermisos y referenciar desde usuario
+    const permiso1Almacenado =  await permiso1.save();
+    const permiso2Almacenado =  await permiso2.save();
 
+    //instancia de Usuario
     const usuario1=  new Usuario(
         { nombre:"Administrador",
          idgrupo: grupoAlmacenado._id,
          permisos:[
-             {permiso: permiso1._id , estado:true},
-             {permiso: permiso2._id , estado:true}
+             {permiso: permiso1Almacenado._id , estado:true},
+             {permiso: permiso2Almacenado._id , estado:true}
          ]
         })
+        //proocedemos almacenar usuario
     const usuarioAlmacenado = await usuario1.save();
-    console.log(usuarioAlmacenado)
-    //todo probar populate en find
+    //  console.log(usuarioAlmacenado)
+    //consultar la información de usuarios
+    const resultado= await Usuario.find()
+    .populate("idgrupo")
+    .populate("permisos.permiso");
+    //mostrar datos de usuario específico
+    console.log(resultado[0].permisos)
+
 })()
